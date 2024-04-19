e8db2ef9ebfcae8d01970a3210d504edf844e48f
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2024-04-18 22:44:48 -0500

    initial refactored commit

---
+++ app/src/main/java/com/example/sharedhouse/FeedViewFragment.kt
@@ -0,0 +1,41 @@
+package com.example.sharedhouse
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sharedhouse.databinding.FeedViewBinding
+
+class FeedViewFragment : Fragment() {
+    companion object {
+        val TAG : String = "SelectFragment"
+    }
+    // https://developer.android.com/topic/libraries/view-binding#fragments
+    private var _binding: FeedViewBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+//    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FeedViewBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+//        Log.d(SimpleFragment.TAG, "onCreateView ${viewModel.selected}")
+        return root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        binding.recyclerView.layoutManager = LinearLayoutManager(binding.recyclerView.context)
+//        binding.recyclerView.adapter = SelectAdapter(viewModel)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/sharedhouse/PaymentsFragment.kt
@@ -0,0 +1,52 @@
+package com.example.sharedhouse
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sharedhouse.databinding.PaymentsFragmentBinding
+
+class PaymentsFragment : Fragment() {
+    companion object {
+        val TAG : String = PaymentsFragment::class.java.simpleName
+    }
+    // https://developer.android.com/topic/libraries/view-binding#fragments
+    private var _binding: PaymentsFragmentBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+//    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = PaymentsFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+//        Log.d(TAG, "onCreateView ${viewModel.selected}")
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // recycler view layout manager, and adapter
+        //SSS
+//        binding.recyclerView.layoutManager = LinearLayoutManager(binding.recyclerView.context)
+//        binding.recyclerView.adapter = SimpleAdapter(viewModel)
+        //EEE // XXX Write me, action bar title,
+        binding.addExpense.setOnClickListener {
+            findNavController().navigate(R.id.addExpenseFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
